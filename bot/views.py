75751c43b19672933c8741bdb90ba5588f52ca35
Husan-Muhiddinov <khusanmukhiddinov43@gmail.com>
2023-04-03 10:42:19 +0500

    innovation bot

--- bot/views.py
+++ bot/views.py
@@ -47,6 +47,31 @@ def start(update: Update, context: CallbackContext):
     log.save()
     update.message.reply_text(f"Assalomu alaykum {user.first_name}, botimizga xush kelibsiz", reply_markup=keyboard_buttons(type='welcome'))
 
+def send_items(update, context, page):
+    a=Ids.objects.filter(status_ID='Buyruq_ID')
+    # print(a)
+# Calculate the start and end index of the items for the current page
+    start_index = (page - 1) * 10
+    end_index = start_index + 10
+    ITEMS=a[start_index:end_index]
+    print(ITEMS)
+    buttons = []
+    for i in range(len(ITEMS)):
+        button = InlineKeyboardButton(str(ITEMS[i]), callback_data=str(ITEMS[i]))
+        buttons.append([button])
+
+    # Add navigation buttons to the bottom of the keyboard
+    prev_button = InlineKeyboardButton('Prev', callback_data=f'prev_{page}')
+    next_button = InlineKeyboardButton('Next', callback_data=f'next_{page}')
+    buttons.append([prev_button, next_button])
+
+    # Create the InlineKeyboardMarkup with the buttons
+    reply_markup = InlineKeyboardMarkup(buttons)
+
+
+    # Send the message with the current page of items and the navigation buttons
+    context.bot.send_message(text=f'Page {page}:', chat_id=update.effective_chat.id, reply_markup=reply_markup)
+
 
 global yangi, tahrir
 yangi = False
@@ -84,19 +109,19 @@ def received_message(update: Update, context: CallbackContext):
     elif msg=="ID berish":
         update.message.reply_text("Yaratilgan ID larni ko'rish", reply_markup=keyboard_buttons(type='BUTTON2'))
     elif msg=="Buyruq ID lar":
-        a=Ids.objects.filter(status_ID='Buyruq_ID')
-        paginator = Paginator(a, 10)
-        page_number = 1
-        page = paginator.page(page_number)
-        buttons = []
-        for item in page:
-           buttons.append([InlineKeyboardButton(item.code_id, callback_data=item.code_id)])
-        if page.has_previous():
-           buttons.append([InlineKeyboardButton("Orqaga", callback_data=f"prev_{page_number}")])
-        if page.has_next():
-           buttons.append([InlineKeyboardButton("Oldinga", callback_data=f"next_{page_number}")])
-        keyboard = InlineKeyboardMarkup(buttons) 
-        update.message.reply_text(text="Choose an item:", reply_markup=keyboard)
+        send_items(update, context, page=1)
+        # paginator = Paginator(a, 10)
+        # page_number = 1
+        # page = paginator.page(page_number)
+        # buttons = []
+        # for item in page:
+        #    buttons.append([InlineKeyboardButton(item.code_id, callback_data=item.code_id)])
+        # if page.has_previous():
+        #    buttons.append([InlineKeyboardButton("Orqaga", callback_data=f"prev_{page_number}")])
+        # if page.has_next():
+        #    buttons.append([InlineKeyboardButton("Oldinga", callback_data=f"next_{page_number}")])
+        # keyboard = InlineKeyboardMarkup(buttons) 
+        # update.message.reply_text(text="Buyruq ID lar:", reply_markup=keyboard)
 
         # log.state['status_buyxat']=True
 
@@ -131,16 +156,16 @@ def received_message(update: Update, context: CallbackContext):
         #     #     parse_mode='Markdown'
         #     # )
     elif msg=="Xat ID lar":
+        # send_items(update, context, page=1)
         a=Ids.objects.filter(status_ID='Xat_ID')
         button1=[]
         for i in range(len(a)):
             button1.append([InlineKeyboardButton(str(a[i]), callback_data=str(a[i]))])
         update.message.reply_text(
-                'ID lar',
+                'Xat ID lar:',
                 reply_markup=InlineKeyboardMarkup(inline_keyboard=button1)
             )
         
-        # log.state['status_buyxat']=True
         
     
 
@@ -159,14 +184,28 @@ def received_message(update: Update, context: CallbackContext):
     elif msg=="Ro'yxat":
         update.message.reply_text("Yaratilgan Ro'yhatlarni ko'rish", reply_markup=keyboard_buttons(type='BUTTON3'))
     elif msg=="Xato":
-        c=Ids.objects.filter(status="Xato")
-        button1=[]
-        for i in range(len(c)):
-            button1.append([InlineKeyboardButton(str(c[i]), callback_data=str(c[i]))])
-        update.message.reply_text(
-                'ID lar',
-                reply_markup=InlineKeyboardMarkup(inline_keyboard=button1)
-            )
+        p=Department.objects.all()
+        list123=[]
+        for i in p:
+            # print(i)
+            c=Ids.objects.filter(status="Xato").filter(userr__name=i)
+            # print(c)
+            k=len(c)
+            a=f"{i} bo'limida : {k} ta"
+            list123.append(a)
+        # print(list123)
+        update.message.reply_text(f"Xatolar: \n\n{l}" for l in list123)
+
+
+            # print(a)
+        # for i in c:
+        # button1=[]
+        # for i in range(len(c)):
+        #     button1.append([InlineKeyboardButton(str(c[i]), callback_data=str(c[i]))])
+        # update.message.reply_text(
+        #         'ID lar',
+        #         reply_markup=InlineKeyboardMarkup(inline_keyboard=button1)
+        #     )
     elif msg=="Imzolanmagan":
         d=Ids.objects.filter(status="Imzolanmagan")
         button1=[]
@@ -185,7 +224,7 @@ def received_message(update: Update, context: CallbackContext):
                 'ID lar',
                 reply_markup=InlineKeyboardMarkup(inline_keyboard=button1)
             )
-    
+     
 
 
     elif yangi == True:
@@ -312,7 +351,7 @@ def received_message(update: Update, context: CallbackContext):
 def callback(update, context):
     user = update.effective_user
     log=Log.objects.filter(user_id=user.id).first()
-    global yangi, bolim
+    global yangi, bolim, o
     msg = update.callback_query.data
     if msg == '+':
         yangi = True
@@ -320,12 +359,13 @@ def callback(update, context):
 
 
     elif msg.isdigit():
+        print(msg)
         a=[str(i) for i in Ids.objects.filter()]
-        print(a)
+        # print(a)
         bolim = a.index(msg)
         log.state["name"]=msg
         keyboard = [[InlineKeyboardButton('Tahrirlash', callback_data='tahrir'), InlineKeyboardButton('O\'chirish', callback_data='ochir'), InlineKeyboardButton('Bo\'lim', callback_data='bol')]]
-        context.bot.send_message(text=f"{msg}", chat_id=update.effective_chat.id, reply_markup=InlineKeyboardMarkup(inline_keyboard=keyboard))
+        o=context.bot.send_message(text=f"{msg}", chat_id=update.effective_chat.id, reply_markup=InlineKeyboardMarkup(inline_keyboard=keyboard))
     elif msg=="tahrir":
         log.state["state"]=1
         tahrir = True
@@ -333,6 +373,9 @@ def callback(update, context):
     elif msg=='ochir':
         b = Ids.objects.filter()
         b[bolim].delete()
+        context.bot.send_message(text="ID muvaffaqiyatli o'chirildi!", chat_id=update.effective_chat.id)
+        context.bot.delete_message(chat_id=update.effective_chat.id, message_id=o.message_id)
+
     elif msg=="bol":
         j=Department.objects.all()
         keyboaard=[]
@@ -347,11 +390,11 @@ def callback(update, context):
         msgg=str(msg.split('_')[1])
         id = Ids.objects.get(code_id=log.state['name'])
         bol = Department.objects.get(name=msgg)
-        print(id)
-        print(bol)
+        # print(id)
+        # print(bol)
         id.userr = bol
         id.save()
-        context.bot.send_message(text="ID bo'limga muvaffaqiyatli saqlandi.✅", chat_id=update.effective_chat.id)
+        context.bot.send_message(text=f"{id} {bol} bo'limiga muvaffaqiyatli saqlandi.✅", chat_id=update.effective_chat.id)
 
 
 
@@ -362,19 +405,22 @@ def callback(update, context):
         for i in range(len(q)):
             button1.append([InlineKeyboardButton(str(q[i]),callback_data=f"butt_{q[i]}")])
         a=context.bot.send_message(
-                text="Bo'limga tegishli ID lar", chat_id=update.effective_chat.id,
+                text=f"{msgg} bo'limiga tegishli ID lar:", chat_id=update.effective_chat.id,
                 reply_markup=InlineKeyboardMarkup(inline_keyboard=button1)
             )
+        
+        
 
 
     elif msg and msg.startswith('butt_'):
-        print(msg)
+        # print(msg)
         mssg=str(msg.split('_')[1])
         log.state["raqam"]=mssg
         b=[str(i) for i in Ids.objects.filter()]
         bolim = b.index(mssg)
         keyboard = [[InlineKeyboardButton('Status', callback_data='stat')]]
         context.bot.send_message(text=f"{mssg}", chat_id=update.effective_chat.id, reply_markup=InlineKeyboardMarkup(inline_keyboard=keyboard))
+        # context.bot.delete_message(chat_id=update.effective_chat.id, message_id=d.message_id)
 
     
 
@@ -395,7 +441,7 @@ def callback(update, context):
         idddd=Ids.objects.get(code_id=log.state['raqam'])
         idddd.status='Xato'
         idddd.save()
-        context.bot.send_message(text="ID ro'yhatga muvaffaqiyatli saqlandi.✅", chat_id=update.effective_chat.id)
+        context.bot.send_message(text="ID Xatolar ro'yxatiga muvaffaqiyatli saqlandi.✅", chat_id=update.effective_chat.id)
 
 
 
@@ -403,7 +449,7 @@ def callback(update, context):
         idddd=Ids.objects.get(code_id=log.state['raqam'])
         idddd.status='Imzolanmagan'
         idddd.save()
-        context.bot.send_message(text="ID ro'yhatga muvaffaqiyatli saqlandi.✅", chat_id=update.effective_chat.id)
+        context.bot.send_message(text="ID Imzolanmaganlar ro'yxatiga muvaffaqiyatli saqlandi.✅", chat_id=update.effective_chat.id)
 
     
 
@@ -411,7 +457,7 @@ def callback(update, context):
         idddd=Ids.objects.get(code_id=log.state['raqam'])
         idddd.status='Foydalanilmagan'
         idddd.save()
-        context.bot.send_message(text="ID ro'yhatga muvaffaqiyatli saqlandi.✅", chat_id=update.effective_chat.id)
+        context.bot.send_message(text="ID Foydalanilmaganlar ro'yxatiga muvaffaqiyatli saqlandi.✅", chat_id=update.effective_chat.id)
 
 
 
@@ -459,8 +505,12 @@ def callback(update, context):
 
 
     
-    elif msg.startswith('next_'):
-        pass
+    elif msg.startswith('prev'):
+        # Send the previous page of items
+        send_items(update, context, page=int(msg[msg.find('_')+1:])-1)
+    elif msg.startswith('next'):
+        # Send the next page of items
+        send_items(update, context, page=int(msg[msg.find('_')+1:])+1)
 
 
 
